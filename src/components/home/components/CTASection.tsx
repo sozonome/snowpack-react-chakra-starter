b815ab97b0dcee54c26d036c1e033b68f4ec086b
Agustinus Nathaniel <berserk.nate@gmail.com>
2021-06-20 22:28:48 +0700

    components(home): add vercel and netlify deploy button to CTA section

--- src/components/home/components/CTASection.tsx
+++ src/components/home/components/CTASection.tsx
@@ -1,4 +1,5 @@
-import { Box, Link } from '@chakra-ui/layout';
+import { Box, Flex, Link } from '@chakra-ui/layout';
+import { Image } from '@chakra-ui/image';
 import { Button } from '@chakra-ui/button';
 import React from 'react';
 import { AiFillGithub } from 'react-icons/ai';
@@ -12,6 +13,25 @@ const CTASection = () => {
       >
         <Button leftIcon={<AiFillGithub />}>Open in Github</Button>
       </Link>
+      <Flex marginY={4} justifyContent="center" gridGap={2}>
+        <Link
+          aria-label="Deploy to Vercel"
+          isExternal
+          href="https://vercel.com/import/git?s=https://github.com/sozonome/snowpack-react-chakra-starter"
+        >
+          <Image src="https://vercel.com/button" alt="Vercel deploy button" />
+        </Link>
+        <Link
+          aria-label="Deploy to Netlify"
+          isExternal
+          href="https://app.netlify.com/start/deploy?repository=https://github.com/sozonome/snowpack-react-chakra-starter"
+        >
+          <Image
+            src="https://www.netlify.com/img/deploy/button.svg"
+            alt="Netlify deploy button"
+          />
+        </Link>
+      </Flex>
     </Box>
   );
 };
